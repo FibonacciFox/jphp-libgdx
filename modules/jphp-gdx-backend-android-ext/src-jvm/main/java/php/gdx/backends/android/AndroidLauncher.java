31ea72fc059002a7857dd219e9ae5b46653f1faa
Max <shamanwolfrus@gmail.com>
2021-04-20 22:50:02 +0300

    UpdateAndroid

--- modules/jphp-gdx-backend-android-ext/src-jvm/main/java/php/gdx/backends/android/AndroidLauncher.java
+++ modules/jphp-gdx-backend-android-ext/src-jvm/main/java/php/gdx/backends/android/AndroidLauncher.java
@@ -1,10 +1,14 @@
 package php.gdx.backends.android;
 
+import android.os.Bundle;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import php.runtime.annotation.Reflection;
 import php.runtime.env.Environment;
 import php.runtime.lang.BaseWrapper;
 import php.runtime.reflection.ClassEntity;
+import android.os.Bundle;
 
 @Reflection.Name("gdx\\backends\\android\\AndroidLauncher")
 public class AndroidLauncher extends BaseWrapper<AndroidApplication> {
@@ -16,6 +20,21 @@ public class AndroidLauncher extends BaseWrapper<AndroidApplication> {
 
     public AndroidLauncher(Environment env, ClassEntity clazz) {
         super(env, clazz);
+    }
+
+    @Reflection.Signature
+    public void __construct() {
+        __wrappedObject = new AndroidApplication();
+    }
+
+    @Reflection.Signature
+    public void initialize(ApplicationAdapter adapter){
+        getWrappedObject().initialize(adapter);
+    }
+
+    @Reflection.Signature
+    public void initialize(ApplicationAdapter adapter, AndroidApplicationConfiguration config){
+        getWrappedObject().initialize(adapter, config);
     }
 
 }
